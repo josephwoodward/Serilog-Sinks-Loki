d22abe2945017c1edb1edff86e9fd4bf4ced161f
Matt <none>
2020-02-13 22:33:53 -0500

    Added "critical" log level name for Log.Fatal() for color coding recognition in Grafana

--- src/Serilog.Sinks.Loki/LokiBatchFormatter.cs
+++ src/Serilog.Sinks.Loki/LokiBatchFormatter.cs
@@ -87,6 +87,9 @@ namespace Serilog.Sinks.Loki
             if (level == LogEventLevel.Information)
                 return "info";
 
+            if (level == LogEventLevel.Fatal)
+                return "critical";
+
             return level.ToString().ToLower();
         }
     }
