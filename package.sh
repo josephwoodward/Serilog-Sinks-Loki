2edae530555ff4681e86fd7633027e33dd21c1bb
Joseph Woodward <joseph.woodward@xeuse.com>
2020-06-05 04:19:10 +0100

    Tag version 2.1.0

--- package.sh
+++ package.sh
@@ -1,1 +1,3 @@
-#!/usr/bin/env bashrm -rf ./packagedotnet pack ./src/Serilog.Sinks.Loki/Serilog.Sinks.Loki.csproj -o ./package/ -c release
+#!/usr/bin/env bash
+rm -rf ./package
+dotnet pack ./src/Serilog.Sinks.Loki/Serilog.Sinks.Loki.csproj -o ./package/ -c release
